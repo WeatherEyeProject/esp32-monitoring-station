9dc2eb4f52516ca227a440e45265ec30c93e26a3
Andrzej Perczak <linux@andrzejperczak.com>
2023-05-06 22:41:33 +0200

    bme680: Use new i2c api

--- src/bme680/sensor_api_glue.cpp
+++ src/bme680/sensor_api_glue.cpp
@@ -11,24 +11,9 @@ BME68X_INTF_RET_TYPE bme68x_i2c_read(uint8_t reg_addr, uint8_t* reg_data, uint32
 	const uint8_t dev_addr = (uint32_t)intf_ptr;
 	auto i2c_bus = sensors_i2c_bus::get_instance();
 
-	if (len == 1) {
-		auto ret = i2c_bus->i2c_read(dev_addr, reg_addr);
-		if (!ret) {
-			return -1;
-		}
+	auto ret = i2c_bus->i2c_read_bytes(dev_addr, reg_addr, reg_data, len);
 
-		*reg_data = *ret;
-	}
-	else {
-		auto ret = i2c_bus->i2c_read_bytes(dev_addr, reg_addr, len);
-		if (!ret) {
-			return -1;
-		}
-
-		memcpy(reg_data, ret->data(), len);
-	}
-
-	return BME68X_INTF_RET_SUCCESS;
+	return ret ? BME68X_INTF_RET_SUCCESS : -1;
 }
 
 BME68X_INTF_RET_TYPE bme68x_i2c_write(uint8_t reg_addr, const uint8_t* reg_data, uint32_t len, void* intf_ptr)
@@ -36,13 +21,7 @@ BME68X_INTF_RET_TYPE bme68x_i2c_write(uint8_t reg_addr, const uint8_t* reg_data,
 	const uint8_t dev_addr = (uint32_t)intf_ptr;
 	auto i2c_bus = sensors_i2c_bus::get_instance();
 
-	bool ret;
-	if (len == 1) {
-		ret = i2c_bus->i2c_write(dev_addr, reg_addr, *reg_data);
-	}
-	else {
-		ret = i2c_bus->i2c_write_bytes(dev_addr, reg_addr, reg_data, len);
-	}
+	bool ret = i2c_bus->i2c_write_bytes(dev_addr, reg_addr, reg_data, len);
 
 	return ret ? BME68X_INTF_RET_SUCCESS : -1;
 }
